cfadd90871c3086564919b5cba8b7a99ed68b466
Lucas Veicir <lucasveicir@gmail.com>
2026-02-20 08:46:59 -0300

    grupo de recompensas

--- sketch.js
+++ sketch.js
@@ -19,6 +19,7 @@ let endImg;
 const PLAY = 1;
 const END = 0;
 let gameState = PLAY;
+let edges;
 
 function preload(){
     pathImg = loadImage("Road.png");
@@ -38,6 +39,10 @@ function setup(){
     boy = createSprite(width /2, height -20);
     boy.addAnimation("run", boyImg);
     boy.scale = 0.1;
+    cashGroup = new Group();
+    diamondsGroup = new Group();
+    jwelleryGroup = new Group();
+    swordGroup = new Group();
 
 }
 
@@ -46,5 +51,45 @@ function draw(){
     if(path.y > height){
         path.y = height/2
     }
+    boy.x = World.mouseX;
+    edges = createEdgeSprites();
+    boy.collide(edges);
+    createCash();
+    createDiamonds();
+    createJwellery();
+
     drawSprites();
+}
+
+function createCash(){
+    if(World.frameCount % 200 == 0){
+        let cash = createSprite(Math.round(random(50,width - 50),40,10,10));
+        cash.addImage(cashImg);
+        cash.scale = 0.12;
+        cash.velocityY = 5;
+        cash.lifetime = 5/height;
+        cashGroup.add(cash);
+    }
+}
+
+function createDiamonds(){
+    if(World.frameCount % 320 == 0){
+        let diamonds = createSprite(Math.round(random(50,width - 50),40,10,10))
+        diamonds.addImage(diamondsImg);
+        diamonds.scale = 0.03
+        diamonds.velocityY = 5;
+        diamonds.lifetime = 5/height;
+        diamondsGroup.add(diamonds);
+    }
+}
+
+function createJwellery(){
+    if(World.frameCount % 410 == 0){
+        let jwellery = createSprite(Math.round(random(50,width - 50),40,10,10));
+        jwellery.addImage(jwelleryImg);
+        jwellery.scale = 0.13;
+        jwellery.velocityY = 5;
+        jwellery.lifetime = 5/height;
+        jwelleryGroup.add(jwellery);
+    }
 }
